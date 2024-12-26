71e1fe4741456469d00582f8428347fcc1315586
abdoubch <la_bouchendouka@esi.dz>
2024-12-27 00:17:13 +0100

    initialisation des models , controller, routes (0 test)

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const { authenticateUser } = require('../middleware/authMiddleware');
+const router = express.Router();
+
+// User routes
+router.post('/', userController.createUser); // Create a user
+router.get('/:id', authenticateUser, userController.getUserById); // Get user by ID
+router.put('/:id', authenticateUser, userController.updateUser); // Update user by ID
+router.delete('/:id', authenticateUser, userController.deleteUser); // Delete user by ID
+
+module.exports = router;
