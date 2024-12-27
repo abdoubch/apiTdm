86d8a4cf33b08a0f8ddabc1b139fb1d148f9a712
abdoubch <la_bouchendouka@esi.dz>
2024-12-27 16:54:11 +0100

    restaurant routes tested (working)

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose');
-const restaurantSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    logo: { type: String },
-    location: {
-      address: { type: String, required: true },
-      coordinates: { type: [Number], index: '2dsphere' },
-    },
-    cuisineType: { type: String, required: true },
-    averageRating: { type: Number, default: 0 },
-    reviewCount: { type: Number, default: 0 },
-    contact: {
-      phone: { type: String },
-      email: { type: String },
-      socialMedia: { type: [String] },
-    },
-  });
+const mongoose = require("mongoose");
 
-    module.exports = mongoose.model('Restaurant', restaurantSchema);
+const restaurantSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  logo: { type: String },
+  location: {
+    address: { type: String, required: true },
+    coordinates: { type: [Number], index: "2dsphere" },
+  },
+  cuisineType: { type: String, required: true },
+  averageRating: { type: Number, default: 0 },
+  reviewCount: { type: Number, default: 0 },
+  contact: {
+    phone: { type: String },
+    email: { type: String },
+    socialMedia: { type: [String] },
+  },
+  meals: [{ type: mongoose.Schema.Types.ObjectId, ref: "Meal" }], // Reference to the Meal model
+});
+
+module.exports = mongoose.model("Restaurant", restaurantSchema);
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -7,5 +7,4 @@ router.post('/', restaurantController.createRestaurant); // Create a restaurant
 router.get('/:id', restaurantController.getRestaurantById); // Get restaurant by ID
 router.put('/:id', restaurantController.updateRestaurant); // Update restaurant by ID
 router.delete('/:id', restaurantController.deleteRestaurant); // Delete restaurant by ID
-
 module.exports = router;
