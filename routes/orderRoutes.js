0b8f87507bfa7c9c47467f1231c1a67ee3262935
HIBA <93740432+HiBa0o0@users.noreply.github.com>
2025-01-04 21:43:38 +0100

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 // Order routes
 router.post("/", orderController.createOrder); // Create an order
+router.get("/", orderController.getAllOrders); // Get an order by user ID
+router.get("/user/:id", orderController.getOrdersByUserId); // Get an order by user ID
 router.get('/:id', orderController.getOrderById); // Get order by ID
 router.put('/:id/status', orderController.updateOrderStatus); // Update order status
 router.delete('/:id', orderController.deleteOrder); // Delete order by ID
