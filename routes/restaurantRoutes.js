36b6617e604bac9d6888ae91cde4e9bbd8f884dc
HIBA <93740432+HiBa0o0@users.noreply.github.com>
2025-01-04 21:43:17 +0100

    Update restaurantRoutes.js

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 // Restaurant routes
 router.post('/', restaurantController.createRestaurant); // Create a restaurant
 router.get('/:id', restaurantController.getRestaurantById); // Get restaurant by ID
+router.get('/', restaurantController.getAllRestaurants); //Get all restaurants
 router.put('/:id', restaurantController.updateRestaurant); // Update restaurant by ID
 router.delete('/:id', restaurantController.deleteRestaurant); // Delete restaurant by ID
 module.exports = router;
